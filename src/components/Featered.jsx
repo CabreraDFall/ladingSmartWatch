a47bd78426c5259a6e5c7be93c9920ebaf7ad894
CabreraDFall <testingdfall@gmail.com>
2024-10-23 18:41:02 +0200

    a

--- src/components/Featered.jsx
+++ src/components/Featered.jsx
@@ -1,9 +1,9 @@
-import { FEATURESRELEASE } from "../dB/FeaturesRelease";
 import Feature from "../utilitys/feature";
+import { FEATURESRELEASE } from "../dB/FeaturesRelease";
 
 function Featered() {
   return (
-    <div id="Features" className="flex-row py-8">
+    <div className="flex-row py-8">
       {FEATURESRELEASE.map((feature, index) => {
         return <Feature key={index} data={feature} />;
       })}
