dba3ee87606906c2923f207a470d8f7ae336372f
CabreraDFall <testingdfall@gmail.com>
2024-10-23 10:29:43 +0200

    fixing

--- src/utilitys/Feature.jsx
+++ src/utilitys/Feature.jsx
@@ -1,19 +1,18 @@
-function Feature() {
-  // const reverse = orientation === "reverse" ? "flex-row-reverse" : "";
+function Feature({ orientation, data }) {
+  const reverse = orientation === "reverse" ? "flex-row-reverse" : "";
 
   return (
-    // <div className={`flex items-center mx-10 ${reverse}`}>
-    //   <div className="flex-1 flex justify-center items-center">
-    //     <img alt="Smart FT" srcSet={data.image} />
-    //   </div>
-    //   <div className="flex-1 p-4 ">
-    //     <div className="mr-10">
-    //       <h3 className="text-5xl font-semibold mb-2 py-5">{data.title}</h3>
-    //       <p className="text-sm text-gray-600 mr-16">{data.content}</p>
-    //     </div>
-    //   </div>
-    // </div>
-    <></>
+    <div className={`flex items-center mx-10 ${reverse}`}>
+      <div className="flex-1 flex justify-center items-center">
+        <img alt="Smart FT" srcSet={data.image} />
+      </div>
+      <div className="flex-1 p-4 ">
+        <div className="mr-10">
+          <h3 className="text-5xl font-semibold mb-2 py-5">{data.title}</h3>
+          <p className="text-sm text-gray-600 mr-16">{data.content}</p>
+        </div>
+      </div>
+    </div>
   );
 }
 
