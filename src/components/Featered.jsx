a08171e1d34bdfa22140dc8111b0829c81cfcae1
CabreraDFall <testingdfall@gmail.com>
2024-10-23 18:58:46 +0200

    Fixing deploy 1

--- src/components/Featered.jsx
+++ src/components/Featered.jsx
@@ -1,9 +1,13 @@
 import { FEATURESRELEASE } from "../dB/FeaturesRelease";
 
 function Featered() {
-  console.log(FEATURESRELEASE);
-
-  return <div>jopa</div>;
+  return (
+    <div className="flex-row py-8">
+      {FEATURESRELEASE.map((feature, index) => {
+        return <div key={index}> holas 3</div>;
+      })}
+    </div>
+  );
 }
 
 export default Featered;
