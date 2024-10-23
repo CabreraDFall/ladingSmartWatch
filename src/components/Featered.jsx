939ff62a71c8900003257d2b99a57adc8971176c
CabreraDFall <testingdfall@gmail.com>
2024-10-23 10:27:34 +0200

    Fixing

--- src/components/Featered.jsx
+++ src/components/Featered.jsx
@@ -1,14 +1,13 @@
-// import { FEATURESRELEASE } from "../dB/FeaturesRelease";
-// import Feature from "../utils/feature";
+import { FEATURESRELEASE } from "../dB/FeaturesRelease";
+import Feature from "../utilitys/feature";
 
 function Featered() {
   return (
-    // <div id="Features" className="flex-row py-8">
-    //   {FEATURESRELEASE.map(({ orientation, ...feature }, index) => {
-    //     return <Feature key={index} data={feature} orientation={orientation} />;
-    //   })}
-    // </div>
-    <div>hoila</div>
+    <div id="Features" className="flex-row py-8">
+      {FEATURESRELEASE.map(({ orientation, ...feature }, index) => {
+        return <Feature key={index} data={feature} orientation={orientation} />;
+      })}
+    </div>
   );
 }
 
