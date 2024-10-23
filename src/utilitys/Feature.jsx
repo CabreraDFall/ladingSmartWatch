536a55053448df78a6680bb810716d2ed57ef044
CabreraDFall <testingdfall@gmail.com>
2024-10-23 10:35:19 +0200

    Eslintrc

--- src/utilitys/Feature.jsx
+++ src/utilitys/Feature.jsx
@@ -4,12 +4,20 @@ function Feature({ orientation, data }) {
   return (
     <div className={`flex items-center mx-10 ${reverse}`}>
       <div className="flex-1 flex justify-center items-center">
-        <img alt="Smart FT" srcSet={data.image} />
+        <img
+          alt={data.title || "Image"}
+          srcSet={data.image}
+          className="w-full h-auto object-contain"
+        />
       </div>
-      <div className="flex-1 p-4 ">
+      <div className="flex-1 p-4">
         <div className="mr-10">
-          <h3 className="text-5xl font-semibold mb-2 py-5">{data.title}</h3>
-          <p className="text-sm text-gray-600 mr-16">{data.content}</p>
+          <h3 className="text-5xl font-semibold mb-2 py-5">
+            {data.title || "Default Title"}
+          </h3>
+          <p className="text-sm text-gray-600 mr-16">
+            {data.content || "Default content goes here."}
+          </p>
         </div>
       </div>
     </div>
